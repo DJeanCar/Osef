a55650fbbbe76488203abd218f2124e23d52bc66
DJeanCar <jean_carlos104@hotmail.com>
2016-08-18 07:49:08 -0500

    first deploy

---
+++ osef/osef/settings/prod.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'osef',
+        'USER': 'osef',
+        'PASSWORD': 'osef',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.child('static')
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1579555865680294'
+SOCIAL_AUTH_FACEBOOK_SECRET = '07b8b7ebf67ee0c72099e4a6fafccb34'
